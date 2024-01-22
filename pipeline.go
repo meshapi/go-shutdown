a179dd1e54e7cd7d33769dba26adf50f333e0ec5
Peyman Mortazavi <pey.mortazavi@gmail.com>
2024-01-21 22:56:38 -0500

    first draft

---
+++ pipeline.go
@@ -0,0 +1,244 @@
+package shutdown
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+// Manager is a shutdown pipeline manager that can be configured to run through a number of parallel and sequencial
+// steps when a shutdown is triggered. In order to start the shutdown procedure upon receiving a kernel Interrupt
+// signal, use WaitForInterrupt() blocking method.
+type Manager struct {
+	steps        []shutdownStep
+	timeout      time.Duration
+	shutdownFunc func()
+	logger       Logger
+	lock         sync.Mutex
+}
+
+// New creates a new shutdown pipeline.
+func New() *Manager {
+	return &Manager{
+		logger: NewStandardLogger(log.Default()),
+	}
+}
+
+// SetTimeout sets the shutdown pipeline timeout. This indicates that when shutdown is triggered, the entire pipeline
+// iteration must finish within the duration specified.
+//
+// NOTE: If the pipeline times out, the shutdown method is still called and some of the steps in the pipeline will
+// still get scheduled but the blocking method (Trigger or WaitForInterrupt) will return immediately without waiting
+// for the rest of the shutdown steps to complete.
+func (m *Manager) SetTimeout(duration time.Duration) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.timeout = duration
+}
+
+// SetShutdownFunc sets a function to get called after all of the shutdown steps have been executed. Regardless of
+// panics or errors, this function will always get executed as the very last step. Even when a the pipeline times out,
+// this function gets called before returning.
+func (m *Manager) SetShutdownFunc(f func()) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.shutdownFunc = f
+}
+
+// SetLogger sets the shutdown logger. If set to nil, no logs will be written.
+func (m *Manager) SetLogger(logger Logger) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.logger = logger
+}
+
+// AddSteps adds parallel shutdown steps. These steps will be executed at the same time together or along
+// with previously added steps if they are also able to run in parallel. In another word, calling AddSteps(a) and
+// AddSteps(b) is same as AddSteps(a, b)
+func (m *Manager) AddSteps(handlers ...NamedHandler) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if len(m.steps) == 0 {
+		m.steps = append(m.steps, shutdownStep{handlers: handlers, parallel: true})
+		return
+	}
+
+	lastStep := m.steps[len(m.steps)-1]
+	if lastStep.parallel {
+		lastStep.handlers = append(lastStep.handlers, handlers...)
+		m.steps[len(m.steps)-1] = lastStep
+		return
+	}
+
+	m.steps = append(m.steps, shutdownStep{handlers: handlers, parallel: true})
+}
+
+// AddSequence adds sequencial steps meaning that these handlers will be executed one at a time and in the same order
+// given.
+// Calling AddSequence(a) and AddSequence(b) is same as AddSequence(a, b)
+func (m *Manager) AddSequence(handlers ...NamedHandler) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if len(m.steps) == 0 {
+		m.steps = append(m.steps, shutdownStep{handlers: handlers, parallel: false})
+		return
+	}
+
+	lastStep := m.steps[len(m.steps)-1]
+	if !lastStep.parallel {
+		lastStep.handlers = append(lastStep.handlers, handlers...)
+		m.steps[len(m.steps)-1] = lastStep
+		return
+	}
+
+	m.steps = append(m.steps, shutdownStep{handlers: handlers, parallel: false})
+}
+
+// AddParallelSequence is similar to AddSequence but it will execute the handlers all at the same time.
+// AddParallelSequence(a) and AddParallelSequence(b) is not the same as AddParallelSequence(a, b). In the former, a
+// runs and upon completion, b starts whereas in the latter case a and b both get started at the same time.
+func (m *Manager) AddParallelSequence(handlers ...NamedHandler) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.steps = append(m.steps, shutdownStep{handlers: handlers, parallel: true})
+}
+
+// Trigger starts the shutdown pipeline immediately. It will acquire a lock on the pipeline so all changes to the
+// pipeline get blocked until the pipeline has completed. Panics and errors are all handled.
+func (m *Manager) Trigger(ctx context.Context) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if len(m.steps) == 0 {
+		return
+	}
+
+	if m.timeout != 0 {
+		newCtx, cancel := context.WithTimeout(ctx, m.timeout)
+		ctx = newCtx
+		defer cancel()
+	}
+
+	errorCount := 0
+	resultChannel := make(chan handlerResult)
+
+	go func() {
+		for _, step := range m.steps {
+			waitGroup := sync.WaitGroup{}
+			waitGroup.Add(len(step.handlers))
+
+			for _, handler := range step.handlers {
+				if step.parallel {
+					go func(h NamedHandler) {
+						m.executeHandler(ctx, h, resultChannel)
+						waitGroup.Done()
+					}(handler)
+				} else {
+					m.executeHandler(ctx, handler, resultChannel)
+					waitGroup.Done()
+				}
+			}
+
+			waitGroup.Wait()
+		}
+		close(resultChannel)
+	}()
+
+	// blocks until the result channel is closed.
+	errorCount = m.processResultChannel(ctx, resultChannel)
+
+	if m.shutdownFunc != nil {
+		m.shutdownFunc()
+	}
+
+	if errorCount > 0 {
+		if m.logger != nil {
+			m.logger.Error(fmt.Sprintf("shutdown pipeline completed with %d errors", errorCount))
+		}
+	} else {
+		if m.logger != nil {
+			m.logger.Info("shutdown pipeline completed with no errors")
+		}
+	}
+}
+
+// processResultChannel receives from the result channel until the channel is closed. This method is blocking. In the
+// end it will return the count of errors. In the event of a context cancellation, this method returns immediately and
+// increases the count by 1.
+func (m *Manager) processResultChannel(ctx context.Context, resultChannel <-chan handlerResult) int {
+	errorCount := 0
+
+	for {
+		select {
+		case result, ok := <-resultChannel:
+			if ok {
+				if result.Err != nil {
+					errorCount++
+					if m.logger != nil {
+						m.logger.Error(result.HandlerName + " shutdown failed: " + result.Err.Error())
+					}
+				} else {
+					if m.logger != nil {
+						m.logger.Info(result.HandlerName + " shutdown completed")
+					}
+				}
+			} else {
+				// channel is closed, that means there is no more handler result to wait for.
+				return errorCount
+			}
+		case <-ctx.Done():
+			if m.logger != nil {
+				m.logger.Error("context canceled")
+			}
+			return errorCount + 1
+		}
+	}
+}
+
+func (m *Manager) executeHandler(ctx context.Context, handler NamedHandler, resultChannel chan<- handlerResult) {
+	var err error
+
+	defer func() {
+		if panicErr := recover(); panicErr != nil {
+			resultChannel <- handlerResult{HandlerName: handler.Name(), Err: fmt.Errorf("panic: %s", panicErr)}
+		} else {
+			resultChannel <- handlerResult{HandlerName: handler.Name(), Err: err}
+		}
+	}()
+
+	err = handler.HandleShutdown(ctx)
+}
+
+// WaitForInterrupt blocks until an interrupt signal is received and all shutdown steps have been executed.
+func (m *Manager) WaitForInterrupt() {
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+	<-exit
+
+	if m.logger != nil {
+		m.logger.Info("received interrupt signal, starting shutdown procedures...")
+	}
+
+	m.Trigger(context.Background())
+}
+
+type shutdownStep struct {
+	handlers []NamedHandler
+	parallel bool
+}
+
+type handlerResult struct {
+	Err         error
+	HandlerName string
+}
---
+++ pipeline_test.go
@@ -0,0 +1,313 @@
+package shutdown_test
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"sort"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/meshapi/go-shutdown"
+)
+
+type LogWrapper struct {
+	Writer io.Writer
+}
+
+func (l LogWrapper) Info(text string) {
+	_, _ = l.Writer.Write([]byte(text))
+}
+
+func (l LogWrapper) Error(text string) {
+	_, _ = l.Writer.Write([]byte(text))
+}
+
+// EventTime is to store at what relative time a process completes or should complete.
+type EventTime map[string]time.Duration
+
+func (e EventTime) String() string {
+	keys := []string{}
+	for key := range e {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	writer := &strings.Builder{}
+	for _, key := range keys {
+		_, _ = fmt.Fprintf(writer, "%s\t%d\n", key, e[key].Milliseconds())
+	}
+	return writer.String()
+}
+
+// MustParseEventTime parsed <name>:<duration>,... format.
+func MustParseEventTime(value string) EventTime {
+	parts := strings.Split(value, ",")
+	result := EventTime{}
+	for _, part := range parts {
+		sections := strings.Split(part, ":")
+		duration, err := time.ParseDuration(sections[1])
+		if err != nil {
+			panic("failed to parse time duration " + sections[1])
+		}
+		result[sections[0]] = duration
+	}
+	return result
+}
+
+// SequenceMonitor captures events and marks their time of execution and asserts if they occurred in a certain time
+// frame.
+type SequenceMonitor struct {
+	events    EventTime
+	startTime time.Time
+	lock      sync.Mutex
+}
+
+// Mark stores the relative time of completion of the given procedure. Needs to be called after StartRecording()
+func (s *SequenceMonitor) Mark(name string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.events[name] = time.Since(s.startTime)
+}
+
+// EventTime returns the current recorded event time.
+func (s *SequenceMonitor) EventTime() EventTime {
+	return s.events
+}
+
+// StartRecording sets the start time and initializes the event time.
+func (s *SequenceMonitor) StartRecording() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.events = EventTime{}
+	s.startTime = time.Now()
+}
+
+// Matches returns whether or not the captured events and their timelines matches the input.
+func (s *SequenceMonitor) Matches(input EventTime) bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if len(s.events) != len(input) {
+		return false
+	}
+
+	for inputKey, inputDuration := range input {
+		expectedDuration, ok := s.events[inputKey]
+		if !ok || !s.approximatelySameDuration(inputDuration, expectedDuration) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// if the duration is accurate to the 10ms precision, then this function returns true.
+func (s *SequenceMonitor) approximatelySameDuration(d1, d2 time.Duration) bool {
+	return (d1.Milliseconds() - d1.Milliseconds()%100) == (d2.Milliseconds() - d2.Milliseconds()%100)
+}
+
+func contextWithMonitor(monitor *SequenceMonitor) context.Context {
+	//nolint:staticcheck
+	return context.WithValue(context.Background(), "monitor", monitor)
+}
+
+// ShutdownMarker is a test shutdown handler that marks the completion of events using the SequenceMonitor from the context.
+type ShutdownMarker struct {
+	name  string
+	err   error
+	panic bool
+}
+
+func (s ShutdownMarker) Name() string {
+	return s.name
+}
+
+func (s ShutdownMarker) HandleShutdown(ctx context.Context) error {
+	monitor := ctx.Value("monitor").(*SequenceMonitor)
+	if monitor == nil {
+		panic("no monitor available")
+	}
+
+	time.Sleep(100 * time.Millisecond)
+	monitor.Mark(s.name)
+	if s.panic {
+		panic("shutdown panic")
+	}
+	return s.err
+}
+
+func TestTriggerRuntime(t *testing.T) {
+	testCases := []struct {
+		Name              string
+		Setup             func(*shutdown.Manager)
+		Timeout           time.Duration
+		ExpectedEventTime EventTime
+	}{
+		{
+			// in this test, since all steps are parallel, they should all complete at the 10ms mark.
+			Name: "AllParallel",
+			Setup: func(m *shutdown.Manager) {
+				m.AddSteps(ShutdownMarker{name: "a"}, ShutdownMarker{name: "b"})
+				m.AddSteps(ShutdownMarker{name: "c"})
+			},
+			ExpectedEventTime: MustParseEventTime("a:100ms,b:100ms,c:100ms"),
+		},
+		{
+			// in this test, the first two are in one parallel group, the second group is also in a parallel group so the
+			// first group should complete around the same time and after that the second group should start and finish
+			// around the same time.
+			Name: "TwoParallelSequences",
+			Setup: func(m *shutdown.Manager) {
+				m.AddSteps(ShutdownMarker{name: "a"}, ShutdownMarker{name: "b"})
+				m.AddParallelSequence(ShutdownMarker{name: "c"}, ShutdownMarker{name: "d"})
+			},
+			ExpectedEventTime: MustParseEventTime("a:100ms,b:100ms,c:200ms,d:200ms"),
+		},
+		{
+			// in this test, the first group runs in parallel and when they're all completed the second group runs in
+			// sequence and one after each other. So a and b complete at the same time, after that c runs and completes then
+			// d runs and completes.
+			Name: "ParallelAndSequence",
+			Setup: func(m *shutdown.Manager) {
+				m.AddSteps(
+					ShutdownMarker{name: "a"},
+					ShutdownMarker{name: "b"})
+				m.AddSequence(
+					ShutdownMarker{name: "c"},
+					ShutdownMarker{name: "d"})
+			},
+			ExpectedEventTime: MustParseEventTime("a:100ms,b:100ms,c:200ms,d:300ms"),
+		},
+		{
+			// in this test, we have an ordered set of steps but one of them errors out.
+			Name: "SequenceWithError",
+			Setup: func(m *shutdown.Manager) {
+				m.AddSequence(
+					ShutdownMarker{name: "a", err: errors.New("failed")},
+					ShutdownMarker{name: "b"})
+			},
+			ExpectedEventTime: MustParseEventTime("a:100ms,b:200ms"),
+		},
+		{
+			// in this test, we have an ordered set of steps but one of them panics.
+			Name: "SequenceWithPanic",
+			Setup: func(m *shutdown.Manager) {
+				m.AddSequence(
+					ShutdownMarker{name: "a", panic: true},
+					ShutdownMarker{name: "b"})
+			},
+			ExpectedEventTime: MustParseEventTime("a:100ms,b:200ms"),
+		},
+		{
+			// in this test, timeout is tested. Shutdown should still get called and the first step should complete but the
+			// second step should no longer block the execution.
+			Name: "SequenceWithTimeout",
+			Setup: func(m *shutdown.Manager) {
+				m.AddSequence(
+					ShutdownMarker{name: "a", err: errors.New("failed")},
+					ShutdownMarker{name: "b"})
+				m.SetTimeout(120 * time.Millisecond) // time to complete a but not b
+			},
+			ExpectedEventTime: MustParseEventTime("a:100ms"),
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.Name, func(t *testing.T) {
+			useLogger := true
+			shutdownCalled := false
+			for i := 0; i < 2; i++ {
+				monitor := &SequenceMonitor{}
+				pipeline := shutdown.New()
+				var logger shutdown.Logger
+				logData := &bytes.Buffer{}
+
+				if useLogger {
+					logger = LogWrapper{Writer: logData}
+					pipeline.SetLogger(logger)
+					useLogger = false
+				} else {
+					pipeline.SetLogger(nil)
+				}
+
+				if !shutdownCalled {
+					pipeline.SetShutdownFunc(func() {
+						shutdownCalled = true
+					})
+				}
+				pipeline.SetTimeout(tt.Timeout)
+
+				tt.Setup(pipeline)
+				monitor.StartRecording()
+				pipeline.Trigger(contextWithMonitor(monitor))
+				if !monitor.Matches(tt.ExpectedEventTime) {
+					t.Logf("log data:\n%s", logData.String())
+					t.Fatalf("expected event time:\n%s\ngot:\n%s", tt.ExpectedEventTime, monitor.EventTime())
+					return
+				}
+				if !shutdownCalled {
+					t.Fatal("shutdown function did not get called")
+				}
+			}
+		})
+	}
+}
+
+func TestDefault(t *testing.T) {
+	var d *shutdown.Manager
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	broken := false
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			mgr := shutdown.Default()
+			if d != nil && mgr != d {
+				broken = true
+			}
+			d = mgr
+		}()
+	}
+	wg.Wait()
+	if broken {
+		t.Fatal("default pointer changed")
+		return
+	}
+
+	monitor := &SequenceMonitor{}
+	monitor.StartRecording()
+
+	shutdownCalled := false
+	shutdown.AddSteps(ShutdownMarker{name: "1"}, ShutdownMarker{name: "2"})
+	shutdown.AddSequence(ShutdownMarker{name: "3"}, ShutdownMarker{name: "4"})
+	shutdown.AddParallelSequence(ShutdownMarker{name: "5"}, ShutdownMarker{name: "6"})
+	shutdown.SetTimeout(350 * time.Millisecond)
+	shutdown.SetLogger(nil)
+	shutdown.SetShutdownFunc(func() {
+		shutdownCalled = true
+	})
+	shutdown.Trigger(contextWithMonitor(monitor))
+	if !shutdownCalled {
+		t.Fatalf("shutdown did not get called")
+	}
+
+	expectation := MustParseEventTime("1:100ms,2:100ms,3:200ms,4:300ms")
+	if !monitor.Matches(expectation) {
+		t.Fatalf("expected:\n%s\ngot:\n%s", expectation, monitor.EventTime())
+	}
+}
+
+func TestHandlerFunc(t *testing.T) {
+	// just make sure the handle functions get compiled.
+	manager := shutdown.New()
+	manager.AddSteps(shutdown.HandlerFuncWithName("no-context-no-error", func() {}))
+	manager.AddSteps(shutdown.HandlerFuncWithName("context-no-error", func(context.Context) {}))
+	manager.AddSteps(shutdown.HandlerFuncWithName("no-context-error", func(context.Context) error { return nil }))
+	manager.AddSteps(shutdown.HandlerFuncWithName("context-error", func(context.Context) error { return nil }))
+}
